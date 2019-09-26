5ebb22307d13ac5d9fb4a1d824e5e5a04cdc438e
Arne Beer <arne@twobeer.de>
2019-09-26 17:10:56 +0200

    cargo fmt

--- src/template.rs
+++ src/template.rs
@@ -1,21 +1,22 @@
-use ::std::collections::HashMap;
-use ::actix_web::HttpResponse;
 use ::actix_web::http::StatusCode;
+use ::actix_web::HttpResponse;
 use ::handlebars::Handlebars;
 use ::log::info;
+use ::std::collections::HashMap;
 
-
-pub fn verify_template_parameters(template: String, params: &HashMap<String, String>) -> Result<(), HttpResponse> {
-
+pub fn verify_template_parameters(
+    template: String,
+    params: &HashMap<String, String>,
+) -> Result<(), HttpResponse> {
     let mut handlebars = Handlebars::new();
     handlebars.set_strict_mode(true);
     info!("Got parameters: {:?}", params);
     let result = handlebars.render_template(&template, params);
 
     match result {
-        Err(error) => {Err(
-            HttpResponse::build(StatusCode::BAD_REQUEST).json(format!("{:?}", error))
-        )},
+        Err(error) => {
+            Err(HttpResponse::build(StatusCode::BAD_REQUEST).json(format!("{:?}", error)))
+        }
         Ok(result) => {
             info!("Template renders properly: {}", result);
             Ok(())
