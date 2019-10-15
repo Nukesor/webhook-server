5883a55df1ffdc0185ca7686969ce1e56d367941
Arne Beer <arne@twobeer.de>
2019-10-15 19:21:47 +0200

    Add shell tests

--- test.sh
+++ test.sh
@@ -26,5 +26,11 @@ echo -n '{"parameters":{"param1":"-al","param2":"/tmp"}}' | http POST localhost:
     X-Hub-Signature:'sha1=d762407ca7fb309dfbeb73c080caf6394751f0a4' \
     Authorization:'Basic dGVzdDp0ZXN0dGVzdA=='
 
+echo -n '{"parameters":{"param1":"-al","param2":"/tmp"}}' | http POST localhost:8000/ls \
+    X-Hub-Signature:'sha1=d762407ca7fb309dfbeb73c080caf6394751f0a4'
+
+echo -n '{"parameters":{"param1":"-al","param2":"/tmp"}}' | http POST localhost:8000/ls \
+    Signature:'sha1=d762407ca7fb309dfbeb73c080caf6394751f0a4'
+
 http GET localhost:8000/lshome \
     Authorization:'Basic dGVzdDp0ZXN0dGVzdA=='
